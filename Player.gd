ea0aa72c7212f08a58f2b9e166cf4bcd2866f085
99p <poccori@gmail.com>
2023-09-27 11:12:21 +0900

    oh yeah

--- Player.gd
+++ Player.gd
@@ -1,20 +1,30 @@
 extends CharacterBody2D
 
+const Bullet = preload("res://bullet.tscn")
 @export var speed: int = 2
 
 func _process(_delta):
-	if Input.is_action_pressed("Left"):
-		position.x += -speed
-	if Input.is_action_pressed("Right"):
-		position.x += speed
-	if Input.is_action_pressed("Up"):
-		position.y += -speed
-	if Input.is_action_pressed("Down"):
-		position.y += speed
+	# if Input.is_action_pressed("Left"):
+	# 	position.x += -speed
+	# if Input.is_action_pressed("Right"):
+	# 	position.x += speed
+	# if Input.is_action_pressed("Up"):
+	# 	position.y += -speed
+	# if Input.is_action_pressed("Down"):
+	# 	position.y += speed
+	position += Input.get_vector("Left", "Right", "Up", "Down") * speed
+
+	if Input.is_action_pressed("Fire") && $FireRateTimer.time_left == 0:
+		fire()
 
 func dead():
 	queue_free()
 
 func _on_get_item(additional_score):
-	print('wow')
 	speed += additional_score / 10
+
+func fire():
+	var bullet = Bullet.instantiate()
+	get_tree().get_root().add_child(bullet)
+	bullet.global_position = global_position
+	$FireRateTimer.start()
